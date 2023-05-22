318960369f2e7c789bddb5d15cdb6745f66ff99e
Austin Garrett <agarrett777@gmail.com>
2023-05-22 19:36:09 -0400

    add: chapter 11 unit tests

--- src/adder.rs
+++ src/adder.rs
@@ -1,3 +1,7 @@
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}", "Carol")
+}
+
 pub fn add_two(a: i32) -> i32 {
     a + 2
 }
@@ -13,15 +17,50 @@ impl Rectangle {
         self.width > other.width && self.height > other.height
     }
 }
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value > 1 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        } else if value < 100 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
+}
+
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn it_works() -> Result<(), String> {
         let result = 2 + 2;
         assert_eq!(result, 4);
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 
     #[test]
@@ -30,7 +69,7 @@ mod tests {
     }
 
     #[test]
-    fn another() {
+    fn deliberate_failure() {
         panic!("Make this test fail");
     }
 
@@ -66,4 +105,51 @@ mod tests {
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
     }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
+    }
+
+    // avoid testing private functions,
+    // better to test public interfaces
+    // unless the private modules have
+    // highly complex functionality
+    // #[test]
+    // fn internal() {
+    //     assert_eq!(4, internal_adder(2, 2));
+    // }
 }
