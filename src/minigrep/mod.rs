ff169649091fc6809218f1e1f51de30b03dea159
Austin Garrett <agarrett777@gmail.com>
2023-05-25 20:20:00 -0400

    add: chapter 12 known failing unit test

--- src/minigrep/mod.rs
+++ src/minigrep/mod.rs
@@ -24,7 +24,30 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // recall "?"" returns either a Result or Option.
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
-
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    for line in contents.lines() {
+        if line.contains(query) {
+            // todo: add line to results
+        }
+    }
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
