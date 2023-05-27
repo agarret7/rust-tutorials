bb852559c6650344fcbfe432fdc5342de7d25879
Austin Garrett <agarrett777@gmail.com>
2023-05-27 01:06:43 -0400

    refactor: use iterator adaptor instead of loop in minigrep

--- src/minigrep/mod.rs
+++ src/minigrep/mod.rs
@@ -53,13 +53,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    // let mut results = Vec::new();
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
+    // iterator adaptor pattern avoids mutable results
+    // and is slightly faster performance-wise
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
